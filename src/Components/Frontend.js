9edc15dae83d7ddd92b368b936209c6de1b77afc
hansitharaka <it19188850@my.sliit.lk>
2021-05-28 11:57:35 +0530

    draft Admin dashboard sidebar added

--- src/Components/Frontend.js
+++ src/Components/Frontend.js
@@ -4,7 +4,10 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './Home/Home';
 import Header from "./Header-Footer/Header";
 import Login from "./Login/Login";
-import AdminDashBoard from "./AdminDashboard/AdminDashBoard";
+import AdminNav from "./Header-Footer/AdminNav";
+import PowerpointTemplates from "./AdminDashboard/Templates/PowerpointTemplates";
+import ResearchTemplates from "./AdminDashboard/Templates/ResearchTemplates";
+import OtherTemplates from "./AdminDashboard/Templates/OtherTemplates";
 // import Footer from "./Header-Footer/Footer";
 
 class Frontend extends Component {
@@ -12,7 +15,6 @@ class Frontend extends Component {
     render() {
         return(
             <div className="Frontend">
-
                 <Router>
 
                     <Header/>
@@ -20,12 +22,14 @@ class Frontend extends Component {
                     <Switch>
                         <Route path="/" exact component={Home}/>
                         <Route path="/Login" exact component={Login}/>
-                        <Route path="/Admindashboard" exact component={AdminDashBoard}/>
+                        <Route path="/Admindashboard" exact component={AdminNav}/>
+                        <Route path="/admin-template/research" exact component={ResearchTemplates}/>
+                        <Route path="/admin-template/powerpoint" exact component={PowerpointTemplates}/>
+                        <Route path="/admin-template/other" exact component={OtherTemplates}/>
                     </Switch>
 
                     {/*<Footer/>*/}
                 </Router>
-
             </div>
         )
     }
