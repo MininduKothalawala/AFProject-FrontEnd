b569b75fa70d842df4c78811c391c4cb735e4288
hansitharaka <it19188850@my.sliit.lk>
2021-06-26 22:52:11 +0530

    added component for admins to view conference details

---
+++ src/Components/AdminDashboard/Conference/ViewConference.js
@@ -0,0 +1,82 @@
+import React, {Component} from "react";
+import axios from "axios";
+
+class ViewConference extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            cid: props.confID,
+            confName: '',
+            description: '',
+            startingDate: '',
+            endingDate: '',
+            venue: ''
+        }
+
+    }
+
+    componentDidMount() {
+        this.refresh()
+    }
+
+    refresh = () => {
+        const id = this.state.cid
+
+        axios.get(`http://localhost:8080/api/conference/conferencebyid/${id}`)
+            .then(response => {
+                // console.log(response.data)
+                this.setState({
+                    confName: response.data.conferenceName,
+                    description: response.data.description,
+                    startingDate: response.data.startingDate,
+                    endingDate: response.data.endingDate,
+                    venue: response.data.venue
+                })
+
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    render() {
+        const {cid, confName, description, startingDate, endingDate, venue} = this.state
+
+        return (
+            <div>
+                    <div className={"px-3 py-2"}>
+                        <div>
+                            <h5>Conference ID</h5>
+                            <p>{cid}</p>
+                        </div> <hr/>
+                        <div>
+                            <h5>Conference Name</h5>
+                            <p>{confName}</p>
+                        </div> <hr/>
+                        <div>
+                            <h5>Description</h5>
+                            <p>{description}</p>
+                        </div> <hr/>
+                        <div>
+                            <h5>Starting Date</h5>
+                            <p>{startingDate}</p>
+                        </div> <hr/>
+                        <div>
+                            <h5>Ending Date</h5>
+                            <p>{endingDate}</p>
+                        </div> <hr/>
+                        <div>
+                            <h5>Venue</h5>
+                            <p>{venue}</p>
+                        </div>
+                    </div>
+            </div>
+        );
+
+    }
+
+}
+
+
+export default ViewConference;
